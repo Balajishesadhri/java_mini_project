20b1d835eb71f8923b6af230a78353472a6db4d7
Balajishesadhri <128590523+Balajishesadhri@users.noreply.github.com>
2023-04-25 13:02:37 +0530

    Add files via upload

---
+++ team.java
@@ -0,0 +1,39 @@
+package Balaji;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class team {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<String> r=new ArrayList<>();
+		String [] one ={ "Yogesh" , "Ajitha" ,"Daisvalin Subak","Prabakaran" , "Priyanka" , "Thiyagu"," SabariBala " ,
+				"Basima" , "Praveen Kumar", "Balaji" , "Amalraj" , "Penasir", "Ibrahim" , "Sangeetha " ,
+				"Arunkumar","Sivaramakrishnan" , "Jayasurya" ," Mugilan" ,"Venkatesan","Ashwini" , "Stalin Sagayaraj","Anjal","Hema"};
+		r.addAll(Arrays.asList(one));
+	Collections.shuffle(r);
+	System.out.println("New Team :");
+	
+	
+	
+				System.out.println(r.get(0)+" -- "+r.get(1)+" -- "+r.get(2)+" -- "+r.get(3)+" -- "+r.get(4));
+				
+				System.out.println(r.get(5)+" -- "+r.get(6)+" -- "+r.get(7)+" -- "+r.get(8)+" -- "+r.get(9));
+				
+				System.out.println(r.get(10)+" -- "+r.get(11)+" -- "+r.get(12)+" -- "+r.get(13)+" -- "+r.get(14));
+			
+				System.out.println(r.get(20)+" -- "+r.get(18)+" -- "+r.get(19)+" -- "+r.get(21));
+				 
+				System.out.println(r.get(15)+" -- "+r.get(16)+" -- "+r.get(17)+" -- "+r.get(22));
+				
+				
+				
+			}
+			
+		}
+		
+		
+
